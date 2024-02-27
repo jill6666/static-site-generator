bdfe05d447d35cd111a3ff01e950592c71f0b224
jillchang <jill.chang@jkos.com>
2024-02-27 18:22:43 +0800

    fix: form image

--- src/uiRenderer/store/form/Image.jsx
+++ src/uiRenderer/store/form/Image.jsx
@@ -1,44 +1,14 @@
-import { Input, Upload, message } from "antd";
+import { Input, Image } from "antd";
 
-const FormImage = ({ value, onChange, name, multiple = false }) => {
-  const uploadProps = { name, multiple, accept: "image" };
-
-  const handleOnChange = (info) => {
-    const { status } = info.file;
-
-    if (status !== "uploading") console.log(info.file, info.fileList);
-
-    if (status === "done") {
-      message.success(`${info.file.name} file uploaded successfully.`);
-    } else if (status === "error") {
-      message.error(`${info.file.name} file upload failed.`);
-    }
-  };
-
-  const customRequest = (context) => {
-    console.log("context: ", context);
-  };
-
-  const handleOnRemove = (info) => {
-    console.log("info: ", info);
-  };
-
+const FormImage = ({ value, onChange }) => {
   return (
-    <div>
-      <Input allowClear value={value} onChange={onChange} />
-      <Upload.Dragger
-        {...uploadProps}
-        onChange={handleOnChange}
-        customRequest={customRequest}
-        onRemove={handleOnRemove}
-        height={100}
-        style={{ marginTop: ".25rem" }}
-      >
-        <p className="ant-upload-text">Drag & Drop your files or Browse. </p>
-      </Upload.Dragger>
-      {value && (
-        <img src={value} style={{ height: "100px", marginTop: ".25rem" }} />
-      )}
+    <div className="flex flex-col gap-2">
+      <Input
+        value={value}
+        onChange={onChange}
+        placeholder="Input the image url."
+      />
+      {value && <Image src={value} width={200} />}
     </div>
   );
 };
