46ac6ec132e729f07931cdda859a43cc61e4c8d3
jillchang <jill.chang@jkos.com>
2024-02-28 01:30:24 +0800

    feat: close all floder

--- src/components/TreeMap/index.jsx
+++ src/components/TreeMap/index.jsx
@@ -1,15 +1,24 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import {
   Tree,
   getBackendOptions,
   MultiBackend,
 } from "@minoru/react-dnd-treeview";
 import { DndProvider } from "react-dnd";
-import { ChevronDownIcon, ChevronRightIcon } from "@radix-ui/react-icons";
+import {
+  ChevronDownIcon,
+  ChevronRightIcon,
+  EyeNoneIcon,
+} from "@radix-ui/react-icons";
 import isArray from "lodash/isArray";
 
 const Dnd = ({ data, active, onClick, onDrop }) => {
+  const ref = useRef(null);
   const [treeData, setTreeData] = useState([]);
+
+  useEffect(() => {
+    handleOpenAll();
+  }, []);
 
   useEffect(() => {
     isArray(data) && setTreeData(data);
@@ -23,43 +32,56 @@ const Dnd = ({ data, active, onClick, onDrop }) => {
   const handleOnClick = (id) => {
     onClick && onClick({ id });
   };
+
+  const handleOpenAll = () => ref.current.openAll();
+  const handleCloseAll = () => ref.current.closeAll();
+
   return (
-    <DndProvider backend={MultiBackend} options={getBackendOptions()}>
-      <Tree
-        initialOpen={true}
-        sort={false}
-        tree={treeData}
-        enableAnimateExpand={true}
-        rootId={0}
-        onDrop={handleDrop}
-        render={(node, { depth, isOpen, onToggle }) => {
-          const droppable = node?.droppable;
-          const isActive = node?.id === active;
+    <>
+      <div className="w-full flex gap-2 justify-end">
+        <EyeNoneIcon className="cursor-pointer" onClick={handleCloseAll} />
+      </div>
+      <DndProvider backend={MultiBackend} options={getBackendOptions()}>
+        <Tree
+          ref={ref}
+          initialOpen={true}
+          sort={false}
+          tree={treeData}
+          enableAnimateExpand={true}
+          rootId={0}
+          onDrop={handleDrop}
+          render={(node, { depth, isOpen, onToggle }) => {
+            const droppable = node?.droppable;
+            const isActive = node?.id === active;
 
-          const props = {
-            onClick: () => handleOnClick(node?.id),
-            style: {
-              padding: "0 4px",
-              paddingLeft: depth * 23,
-              color: isActive ? "#555" : "",
-              fontWeight: isActive ? "bold" : "",
-              backgroundColor: isActive ? "#FFF4A2" : "",
-            },
-          };
+            const props = {
+              onClick: () => {
+                handleOnClick(node?.id);
+                onToggle();
+              },
+              style: {
+                padding: "0 4px",
+                paddingLeft: depth * 23,
+                color: isActive ? "#555" : "",
+                fontWeight: isActive ? "bold" : "",
+                backgroundColor: isActive ? "#FFF4A2" : "",
+              },
+            };
 
-          return (
-            <div className="w-full flex items-center" {...props}>
-              {droppable && (
-                <span className="pr-2" onClick={onToggle}>
-                  {isOpen ? <ChevronDownIcon /> : <ChevronRightIcon />}
-                </span>
-              )}
-              {node?.text}
-            </div>
-          );
-        }}
-      />
-    </DndProvider>
+            return (
+              <div className="w-full flex items-center" {...props}>
+                {droppable && (
+                  <span className="pr-2" onClick={onToggle}>
+                    {isOpen ? <ChevronDownIcon /> : <ChevronRightIcon />}
+                  </span>
+                )}
+                {node?.text}
+              </div>
+            );
+          }}
+        />
+      </DndProvider>
+    </>
   );
 };
 
