6dd2fdd5ada97acbc158f8b70d4b7d32c550d3d4
jill.chang <jillchang05@gmail.com>
2024-03-22 13:48:56 -0700

    feat: intro style and catch error

--- src/app/containers/Intro.jsx
+++ src/app/containers/Intro.jsx
@@ -5,7 +5,7 @@ import { NotionRenderer } from 'react-notion';
 
 const Intro = () => {
   const [blockMap, setBlockMap] = useState([]);
-  const NOTION_PAGE_IG = 'CUBE-f4e53fd4c9544f709b535ab1ffaf420b?pvs=4';
+  const NOTION_PAGE_ID = 'CUBE-f4e53fd4c9544f709b535ab1ffaf420b?pvs=4';
 
   useEffect(() => {
     getBlockMap().then(data => {
@@ -14,15 +14,15 @@ const Intro = () => {
   }, []);
 
   const getBlockMap = async () => {
-    const data = await fetch(`https://notion-api.splitbee.io/v1/page/${NOTION_PAGE_IG}`).then(res => res.json());
+    const data = await fetch(`https://notion-api.splitbee.io/v1/page/${NOTION_PAGE_ID}`)
+      .then(res => res.json())
+      .catch(e => console.error(JSON.stringify(e)));
     return data;
   };
 
   return (
-    <div className="w-full">
-      <div className="max-w-[800px] px-8 border m-auto">
-        <NotionRenderer blockMap={blockMap} />
-      </div>
+    <div className="max-w-[800px] p-8 m-auto min-h-screen">
+      <NotionRenderer blockMap={blockMap} />
     </div>
   );
 };
