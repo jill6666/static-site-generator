a4a775ad60b39b235bfe40fe4569ec6b9f509778
jill.chang <jillchang05@gmail.com>
2024-03-24 14:35:17 -0700

    feat: buymeacoffee style

--- src/uiRenderer/store/web/BuyMeACoffee/index.jsx
+++ src/uiRenderer/store/web/BuyMeACoffee/index.jsx
@@ -3,7 +3,8 @@ import { Button as ButtonMarkup } from '../Button/markup';
 import { ReloadIcon, LinkBreak2Icon } from '@radix-ui/react-icons';
 import { useSendTransaction, useAccount, useConnect, useDisconnect } from 'wagmi';
 import { parseEther } from 'viem';
-import { Modal, message } from 'antd';
+import { Modal, message, Input } from 'antd';
+import EthIcon from './eth.svg';
 
 const BuyMeACoffee = ({
   name,
@@ -21,6 +22,7 @@ const BuyMeACoffee = ({
   borderRadius,
   isLoading,
   ownerAddr, // must be 0x-prefixed
+  min = 0.000001,
   ...props
 }) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -62,50 +64,61 @@ const BuyMeACoffee = ({
     message.success('Disconnected!');
   };
   return (
-    <div className="w-full flex justify-between items-center">
-      <input type="text" value={value} onChange={handleOnChange} />
-      <ButtonMarkup
-        size={size}
-        variant={variant}
-        className={twStyle}
-        style={{
-          color,
-          background,
-          width,
-          height,
-          padding,
-          margin,
-          border,
-          borderRadius,
-        }}
-        onClick={handleSupportOnClick}
-        {...props}
-      >
-        {isPending ? <ReloadIcon className="animate-spin" /> : 'Support'}
-      </ButtonMarkup>
-      {isConnected && <LinkBreak2Icon onClick={handleDisconnect} width={24} height={24} className="cursor-pointer" />}
+    <>
+      <div className="w-full flex justify-between items-center p-2 rounded-md gap-2">
+        <Input
+          type="number"
+          className="h-auto py-1"
+          prefix={<img src={EthIcon} />}
+          suffix="ETH"
+          min={min}
+          value={value}
+          step={0.000001}
+          onChange={handleOnChange}
+        />
+        <ButtonMarkup
+          size={size}
+          variant={variant}
+          className={twStyle}
+          style={{
+            color,
+            background,
+            width,
+            height,
+            padding,
+            margin,
+            border,
+            borderRadius,
+          }}
+          onClick={handleSupportOnClick}
+          {...props}
+        >
+          {isPending ? <ReloadIcon className="animate-spin" /> : 'Support'}
+        </ButtonMarkup>
+        {isConnected && <LinkBreak2Icon onClick={handleDisconnect} width={24} height={24} className="cursor-pointer" />}
 
-      {/* Connect Wallect Modal */}
-      <Modal
-        okButtonProps={{ type: 'default' }}
-        title="Connect Wallet"
-        open={isModalOpen}
-        onOk={handleOnSupport}
-        onCancel={() => setIsModalOpen(false)}
-        footer={(_, { CancelBtn }) => (
-          <div className="flex gap-2">
-            <CancelBtn />
-            {connectors.map(connector => (
-              <ButtonMarkup key={connector.uid} onClick={() => handleWalletConnect({ connector })} type="button">
-                {connector.name}
-              </ButtonMarkup>
-            ))}
-          </div>
-        )}
-      >
-        <p>Connect a wallet to support.</p>
-      </Modal>
-    </div>
+        {/* Connect Wallect Modal */}
+        <Modal
+          okButtonProps={{ type: 'default' }}
+          title="Connect Wallet"
+          open={isModalOpen}
+          onOk={handleOnSupport}
+          onCancel={() => setIsModalOpen(false)}
+          footer={(_, { CancelBtn }) => (
+            <div className="flex gap-2">
+              <CancelBtn />
+              {connectors.map(connector => (
+                <ButtonMarkup key={connector.uid} onClick={() => handleWalletConnect({ connector })} type="button">
+                  {connector.name}
+                </ButtonMarkup>
+              ))}
+            </div>
+          )}
+        >
+          <p>Connect a wallet to support.</p>
+        </Modal>
+      </div>
+    </>
   );
 };
 export default BuyMeACoffee;
