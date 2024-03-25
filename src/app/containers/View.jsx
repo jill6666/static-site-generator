df204af2c98ce23a60a0dd44b1a646f624d7e692
jill.chang <jillchang05@gmail.com>
2024-03-25 16:10:01 -0700

    fix: background of View

--- src/app/containers/View.jsx
+++ src/app/containers/View.jsx
@@ -2,28 +2,32 @@ import { Suspense, useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import RenderSchema from '../../uiRenderer';
 import get from 'lodash/get';
-import getPageData from '../data/firebase/getPageData';
+import getDBPageData from '../data/firebase/getPageData';
 
 const View = () => {
   const [schema, setSchema] = useState([]);
+  const [pageSettings, setPageSettings] = useState({});
   const { pageId } = useParams();
 
   useEffect(() => {
     const init = async () => {
-      const data = await getSchema(pageId);
-      setSchema(data);
+      const { schema, settings } = await getPageData(pageId);
+      setSchema(schema);
+      setPageSettings(settings);
     };
     init();
   }, []);
 
-  const getSchema = async pageId => {
-    const data = await getPageData({ slug: pageId });
+  const getPageData = async pageId => {
+    const data = await getDBPageData({ slug: pageId });
+    const schema = get(data, 'schema', []);
+    const settings = get(data, 'settings', {});
 
-    return get(data, 'schema', []);
+    return { schema, settings };
   };
 
   return (
-    <div className="w-full h-full" style={{}}>
+    <div className="w-full h-full" style={{ background: pageSettings?.backgroundColor }}>
       <Suspense fallback={<>OOPS! Something went wrong</>}>
         <div className="max-w-[480px] m-auto">{RenderSchema({ schema })}</div>
       </Suspense>
